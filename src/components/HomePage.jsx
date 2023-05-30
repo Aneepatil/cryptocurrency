8e83e4a002206691994f460af1fd21647e765456
Aneepatil <anilspatil8@gmail.com>
2023-05-30 22:56:29 +0530

    crypto

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Typography, Row, Col, Statistic } from "antd";
+const { Title } = Typography;
+
+const HomePage = () => {
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Crypto Currencies" value="5"></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total  Exchanges" value="5"></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5"></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Valume" value="5"></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5"></Statistic>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeadrs = {
+  "X-RapidAPI-Key": "b4be63e685mshe606193a03883c3p1a5236jsnf4d2b1017704",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baeUrl = 'https://coinranking1.p.rapidapi.com'
+
+
